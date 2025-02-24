4875a9c42c36ba487013a05ea01560e94b276348
MACTEP <book_master00@proton.me>
2025-02-24 18:50:56 +0300

    Delete backend directory

--- backend/src/bot/utils/isOwner.js
+++
@@ -1,7 +0,0 @@
-import {getUserByTelegramId} from "../../db/application.js"
-
-export const isOwner = async (ctx) => {
-    const user = await getUserByTelegramId(ctx.from.id)
-    return user.role === 'owner'
-}
-
